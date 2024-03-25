a6aec5571084ec5d052daff5b178d15c0e1f51d7
PereAlbertiAguilo <p.albertiaguilo@gmail.com>
2024-03-25 12:54:53 +0100

    end2

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const navbarDropdownBtn = document.querySelectorAll(".navbarDropdownBtn");
 const navbarDropdownMenu = document.querySelector(".navbarDropdownMenu");
+const navbarContainer = document.querySelector(".navbarContainer");
 
 window.onresize = CloseDropdown;
 window.onscroll = CloseDropdown;
+
+// Navabar hide on scroll
+
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", () => {
+  var scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    navbarContainer.style.transform = "translateY(-80px)";
+  } else {
+    navbarContainer.style.transform = "translateY(0px)";
+  }
+  lastScrollTop = scrollTop;
+});
+
+// Navabar dropdown open close
 
 navbarDropdownBtn.forEach((btn) => {
   btn.addEventListener("click", () => {

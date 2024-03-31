3af54329f4142b97ed218cca8e4ffa5e2c8fc17f
PereAlbertiAguilo <p.albertiaguilo@gmail.com>
2024-03-31 19:28:15 +0200

    languages

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-const navbarDropdownBtn = document.querySelectorAll(".navbarDropdownBtn");
-const navbarDropdownMenu = document.querySelector(".navbarDropdownMenu");
-const navbarContainer = document.querySelector(".navbarContainer");
-
 window.onresize = CloseDropdown;
 window.onscroll = CloseDropdown;
 
+// Language changer
+
+const flags = document.getElementById("flags");
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (language) => {
+  const requestJson = await fetch("./languages/" + language + ".json");
+  const texts = await requestJson.json();
+
+  for (let textTochange of textsToChange) {
+    const section = textTochange.dataset.section;
+    const value = textTochange.dataset.value;
+
+    textTochange.innerHTML = texts[section][value];
+  }
+};
+
+flags.addEventListener("click", (e) => {
+  changeLanguage(e.target.parentElement.dataset.language);
+});
+
 // Navabar hide on scroll
+
+const navbarContainer = document.querySelector(".navbarContainer");
 
 var lastScrollTop = 0;
 
@@ -21,6 +41,9 @@ window.addEventListener("scroll", () => {
 });
 
 // Navabar dropdown open close
+
+const navbarDropdownBtn = document.querySelectorAll(".navbarDropdownBtn");
+const navbarDropdownMenu = document.querySelector(".navbarDropdownMenu");
 
 navbarDropdownBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
